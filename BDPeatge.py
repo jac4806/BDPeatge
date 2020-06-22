0410d40a4d0f9a0e05359fd213fd0842b12c6728
jac4806 <jac4806@gmail.com>
2020-06-22 12:38:05 +0200

    Eliminamos ramas Metro y Casa dejandoo una unica , Desarrollo

--- BDPeatge.py
+++ BDPeatge.py
@@ -17,22 +17,30 @@ class Form(QDialog, form_class):
         self.setupUi(self)
         self.fillGrupos()
         self.CB_Grupo.currentIndexChanged.connect(self.fillDescripcion)
-
+        self.B_Tabla.clicked.connect(self.prueba)
 
 
         self.B_Salir.clicked.connect(self.fn_salir)
 
-    def fillDescripcion(self):
+    def prueba(self):
         grupo=self.CB_Grupo.currentText()
         self.E_Observaciones.setPlainText(grupo)
 
+    def fillDescripcion(self):
+        grupo=self.CB_Grupo.currentText()
+        Base.execute("SELECT Descripcion FROM Stock WHERE Grupo=grupo")
+        Descripciones=Cursor.fetchall()
+        print(Descripciones)
+        for des in Descripciones:
+            self.CB_Descripcion.addItem(des) 
+        self.CB_Descripcion.setCurrentIndex(-1)    
+            
     def fillGrupos(self):
         texto=open("grupos.txt","r") # Leemos el fichero
         lista=texto.readlines() # Convertimos el texto en lista
         texto.close()
         for i in lista:
            self.CB_Grupo.addItem(i) 
-
         self.CB_Grupo.setCurrentIndex(-1)
 
     def fn_salir(self,event):
